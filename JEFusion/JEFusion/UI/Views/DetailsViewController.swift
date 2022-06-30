bb90c7ff7b15d3b8aecc0484d6e17c1787153aba
Tan Tran <tantt.dev@gmail.com>
2022-06-30 11:27:29 -0400

    Extract `DetailsViewModel`

--- JEFusion/JEFusion/UI/Views/DetailsViewController.swift
+++ JEFusion/JEFusion/UI/Views/DetailsViewController.swift
@@ -9,36 +9,6 @@ import Foundation
 import UIKit
 import Combine
 
-class DetailsViewModel {
-    private var loader: BusinessLoader
-    private var store: BusinessStore
-    var business: BusinessModel
-    @Published var reviews: [Review] = []
-    private var cancellables = Set<AnyCancellable>()
-    
-    init(loader: BusinessLoader, store: BusinessStore, business: BusinessModel) {
-        self.loader = loader
-        self.store = store
-        self.business = business
-    }
-    
-    func fetchReviews() {
-        loader.fetchBusinessReviews(with: business.id)
-            .receive(on: DispatchQueue.main, options: .none)
-            .sink(receiveCompletion: { _ in }) { [weak self] reviews in
-                self?.reviews = Array(reviews.prefix(3))
-            }.store(in: &cancellables)
-    }
-    
-    func updateLikeModel() {
-        guard let isLiked = business.isLiked else { return }
-        self.store.updateLikeModel(business.id, isLiked: isLiked)
-            .sink(receiveCompletion: { _ in }, receiveValue: { success in
-                print("Updating \(success)")
-            }).store(in: &cancellables)
-    }
-}
-
 class DetailsViewController: UIViewController {
     lazy var scrollView: UIScrollView = {
         let scrollView = UIScrollView()
---
+++ JEFusion/JEFusion/UI/Views/DetailsViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  DetailsViewModel.swift
+//  JEFusion
+//
+//  Created by Tan Tan on 6/30/22.
+//
+
+import Foundation
+import Combine
+
+class DetailsViewModel {
+    private var loader: BusinessLoader
+    private var store: BusinessStore
+    var business: BusinessModel
+    @Published var reviews: [Review] = []
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(loader: BusinessLoader, store: BusinessStore, business: BusinessModel) {
+        self.loader = loader
+        self.store = store
+        self.business = business
+    }
+    
+    func fetchReviews() {
+        loader.fetchBusinessReviews(with: business.id)
+            .receive(on: DispatchQueue.main, options: .none)
+            .sink(receiveCompletion: { _ in }) { [weak self] reviews in
+                self?.reviews = Array(reviews.prefix(3))
+            }.store(in: &cancellables)
+    }
+    
+    func updateLikeModel() {
+        guard let isLiked = business.isLiked else { return }
+        self.store.updateLikeModel(business.id, isLiked: isLiked)
+            .sink(receiveCompletion: { _ in }, receiveValue: { success in
+                print("Updating \(success)")
+            }).store(in: &cancellables)
+    }
+}
